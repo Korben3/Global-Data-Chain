cea7555530b297711fea2c638a4015cb65f7eb6b
Korben3 <42899281+Korben3@users.noreply.github.com>
2019-12-01 17:57:49 +0100

    feat: get accounts

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,3 +4,6 @@ import { nodes } from "../config/config.json";
 const client = new APIClient(nodes);
 
 export const getTransactions = options => client.transactions.get(options);
+
+// get latest accounts, preferred way is only retrieve accounts with a valid operator asset
+export const getAccounts = options => client.accounts.get(options);
