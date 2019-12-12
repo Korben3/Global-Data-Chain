15d834e3bd52e83954fe6b9d56d85a6e10b9c9c6
Korben3 <42899281+Korben3@users.noreply.github.com>
2019-12-12 22:47:55 +0100

    feat: click on a marker in worldmap changes linechart

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import WorldMap from "./components/WorldMap";
 import Header from "./components/Header";
@@ -6,11 +6,19 @@ import Statistics from "./components/Statistics";
 import About from "./components/About";
 
 const App = () => {
+  const [operator, setOperator] = useState("korben3");
+  const [account, setAccount] = useState("5320901975065898377L");
+
+  const markerClicked = data => {
+    setOperator(data[0]);
+    setAccount(data[1]);
+  };
+
   return (
     <div>
       <Header />
-      <WorldMap />
-      <Statistics />
+      <WorldMap markerClicked={markerClicked} />
+      <Statistics operator={operator} account={account} />
       <About />
     </div>
   );
