2675223a14be1269880f2c7f3399e503684b9b66
Korben3 <42899281+Korben3@users.noreply.github.com>
2019-12-12 23:14:56 +0100

    feat: added operator details window

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -4,48 +4,39 @@ import LineChart from "./LineChart";
 import { getAccounts } from "../utils/api";
 
 const Statistics = props => {
-  const [operator, setOperator] = useState("korben3");
-  const [account, setAccount] = useState("5320901975065898377L");
-  const [GDCoperators, setGDCoperators] = useState();
-
-  const viewOperatorData = data => {
-    setOperator(data[0]);
-    setAccount(data[1]);
-  };
+  const { operator, account } = props;
+  const [operatorDetails, setOperatorDetails] = useState();
 
   useEffect(() => {
     getAccounts({
-      limit: 7
+      address: account
     })
       .then(res => {
-        console.log("Statistics, accounts:");
+        console.log("stats:");
+        console.log(res);
+        const data = res.data[0];
 
-        const data = res.data.filter(item => item.asset.operator);
-        const GDCoperators = data.map(item => (
-          <li key={item.asset.operator.name}>
-            Operator:
-            <span
-              onClick={() => viewOperatorData([item.asset.operator.name, item.address])}
-              className="operatorButton"
-            >
-              {item.asset.operator.name}
-            </span>
-          </li>
-        ));
-        console.log(GDCoperators);
-
-        setGDCoperators(GDCoperators);
+        const operatorDetails = (
+          <div>
+            <li key={data.asset.operator.name}>Operator: {data.asset.operator.name}</li>
+            <li key={data.address}>Account: {data.address}</li>
+            <li key={data.asset.operator.totalDataTransactions}>
+              Total data transactions: {data.asset.operator.totalDataTransactions}
+            </li>
+          </div>
+        );
+        setOperatorDetails(operatorDetails);
       })
       .catch(err => {
         console.log(err);
       });
-  }, []);
+  }, [account]);
 
   return (
     <div className="StatisticsWrapper">
       <div id="GDCoperators" className="statisticsMobile StatisticsTable1">
-        <code>Click on an operator to view the noise measurements:</code>
-        <ul>{GDCoperators}</ul>
+        <code>Click on a marker to view the operators details.</code>
+        <ul>{operatorDetails}</ul>
       </div>
       <div id="operatorData" className="statisticsMobile StatisticsTable2">
         <LineChart operator={operator} account={account} />
